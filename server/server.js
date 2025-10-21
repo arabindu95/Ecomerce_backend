90e3e3b0eeb66cd3a17301c892bef47b16132592
Arabindu <arabindu123@gmail.com>
2025-10-21 13:04:58 +0530

    update

--- server/server.js
+++ server/server.js
@@ -34,8 +34,12 @@ app.use(
       }
     },
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+app.options("*", cors());
 
 app.use(express.json());
 app.use(cookieParser());
